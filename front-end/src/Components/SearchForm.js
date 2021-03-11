6f4ced1ff7fd5befbcc5ca661af6511e7f9c2968
PanosMits <panag.mitseas@gmail.com>
2021-03-11 16:43:42 +0000

    Create SearchForm Component

---
+++ front-end/src/Components/SearchForm.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import patterns from '../Repositories/Patterns';
+
+const SearchForm = () => {
+    const history = useHistory();
+    const [pattern, setPattern] = useState(patterns[0].value);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const candlestick = { pattern };
+        history.push(`/${candlestick.pattern}`);
+    }
+
+    return (
+        <div className="search-form">
+            <h2>Search for indicators</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Candlestick pattern</label>
+                <select value={pattern.value} onChange={(e) => { setPattern(e.target.value) }}>
+                    {patterns.map((datum) => (
+                        <option value={datum.value} key={datum.id}>
+                            {datum.name}
+                        </option>
+                    ))}
+                </select>
+                <button>Search</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchForm;
