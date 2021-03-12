5b4f077f3be77521a46fb3d8aff8d670359d004d
PanosMits <panag.mitseas@gmail.com>
2021-03-12 11:17:03 +0000

    ResultsTable - Filter out stocks that don't have indications for current pattern

--- front-end/src/Components/ResultsTable.js
+++ front-end/src/Components/ResultsTable.js
@@ -1,25 +1,35 @@
 const ResultsTable = ({ stocks, currentPattern }) => {
+    const filteredStocks = Object.values(stocks).filter((stock) => {
+        return stock[currentPattern];
+    });
+
+    if (filteredStocks.length === 0) {
+        return (
+            <div className="jumbotron">
+                {`Sorry, no ${currentPattern} results detected`}
+            </div>
+        );
+    }
+
     return (
         <div className="jumbotron">
             <table>
-                <tr>
-                    <th>Symbol</th>
-                    <th>Company name</th>
-                    <th>Signal</th>
-                </tr>
-                {
-                    Object.values(stocks).map((stock) => {
-                        if (stock[currentPattern]) {
-                            return (
-                                <tr>
-                                    <td>{stock.symbol}</td>
-                                    <td>{stock.company}</td>
-                                    <td>{stock[currentPattern]}</td>
-                                </tr>
-                            )
-                        }
-                    })
-                }
+                <tbody>
+                    <tr>
+                        <th>Symbol</th>
+                        <th>Company name</th>
+                        <th>Signal</th>
+                    </tr>
+                    {
+                        filteredStocks.map((stock, index) =>
+                            <tr key={index}>
+                                <td>{stock.symbol}</td>
+                                <td>{stock.company}</td>
+                                <td>{stock[currentPattern]}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
             </table>
         </div>
     )
