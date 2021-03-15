9cea7641e39e9caaa2ce7cca72396bb568029faf
PanosMits <panag.mitseas@gmail.com>
2021-03-15 12:31:58 +0000

    Add functions for loading the Historical price data

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,26 +1,64 @@
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 import useFetch from '../Hooks/useFetch';
 import SearchForm from '../Components/SearchForm';
 import ResultsTable from '../Components/ResultsTable';
 import Loader from '../Components/Loader';
+import Button from '../Components/Button';
 
 const Home = () => {
     const { pattern } = useParams();
-    const { error, isLoading, data: patternResults } = useFetch(`/api/get-technical-analysis/${pattern}/`);
+    const { error: patternErrorMessage, isLoading: patternsLoading, data: patternResults } = useFetch(`/api/get-technical-analysis/${pattern}/`);
+    const [pricesLoading, setPricesLoading] = useState(false);
+    const [message, setMessage] = useState(null);
+
+    const loadSnPData = async () => {
+        setPricesLoading(true);
+        setMessage(null);
+        await fetch('/api/snapshot/companies')
+            .then(response => {
+                if (!response.ok) {
+                    setMessage('An error occurred, please try again later');
+                    setPricesLoading(false);
+                }
+            });
+    }
+
+    const loadCryptoData = async () => {
+        setPricesLoading(true);
+        setMessage(null);
+        await fetch('/api/snapshot/cryptos')
+            .then(response => {
+                if (!response.ok) {
+                    setMessage('An error occurred, please try again later');
+                    setPricesLoading(false);
+                }
+            });
+    }
 
     return (
         <div>
-            <div className="jumbotron">
-                <SearchForm />
+            <div className="row">
+                <div className="col-10">
+                    <div className="jumbotron">
+                        <SearchForm />
+                    </div>
+                    {patternsLoading && <Loader />}
+
+                    {pricesLoading && <Loader />}
+                    {pricesLoading && <div>{'Loading prices, this will take a while but you only need to run it once a day'}</div>}
+
+                    {patternResults && <ResultsTable stocks={patternResults.stocks} currentPattern={patternResults.current_pattern} />}
+
+                    {patternErrorMessage && patternResults && <div>{patternErrorMessage}</div>}
+
+                    {message && <div>{message}</div>}
+                </div>
+                <div className="col-2">
+                    <Button text={'Load S&P500 data'} onClick={loadSnPData} />
+                    <Button text={'Load Cryptos data'} onClick={loadCryptoData} />
+                </div>
             </div>
-
-            {isLoading && <Loader />}
-            {isLoading && <Loader />}
-            {isLoading && <Loader />}
-
-            {patternResults && <ResultsTable stocks={patternResults.stocks} currentPattern={patternResults.current_pattern} />}
-
-            { error && patternResults && <div>{error}</div>}
         </div >
     )
 }
